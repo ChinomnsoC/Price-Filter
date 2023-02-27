647d2dd069d7e0d648fcfce60305fde2b53f6ac9
ChinomnsoC <chinomnsoc@gmail.com>
2023-02-27 04:09:03 +0000

    added scapy to project, getting the 403 error, will try a proxy in next commit

--- src/retriever/retriever.py
+++ src/retriever/retriever.py
@@ -1,10 +1,16 @@
-from bs4 import BeautifulSoup
-import requests
+import scrapy
 
-
-response = requests.get('https://www.currys.co.uk/products/lg-oled55cs6la-55-smart-4k-ultra-hd-hdr-oled-tv-with-google-assistant-and-amazon-alexa-10242981.html')
-response.status_code
-
-url = 'https://www.currys.co.uk/products/lg-oled55cs6la-55-smart-4k-ultra-hd-hdr-oled-tv-with-google-assistant-and-amazon-alexa-10242981.html'
-prod_page = BeautifulSoup(url)    
-print(prod_page.prettify())
+class PriceSpider(scrapy.Spider):
+    name = 'TvPrices'
+    start_urls = [
+        'https://www.currys.co.uk/products/lg-oled55cs6la-55-smart-4k-ultra-hd-hdr-oled-tv-with-google-assistant-and-amazon-alexa-10242981',
+    ]
+    def parse(self, response):
+        for prices in response.css('div.price'):
+            yield {
+                'price-information': price.xpath('span/span'),
+                'infos': price.css('span.value::content').get(),
+            }
+        next_page = response.css('li.next a::attr("href")').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
