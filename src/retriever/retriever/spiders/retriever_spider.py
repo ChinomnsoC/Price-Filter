5c18f922250d5bea341f186fa7e223ecb3cf5940
Chinomnso Chinedum <67716309+ChinomnsoC@users.noreply.github.com>
2025-03-26 21:42:19 +0000

    Feature calculator (#6)
    * edited requirement.txt file in new branch
    
    * switched to poetry, removed requirement.txt file, updated readme
    
    * added a make file to rule the whole thing
    
    * Poetry switch and make now scraping successfully
    
    * formatting
    
    * Added overwrite functionality
    
    * Showing git commands

--- src/retriever/retriever/spiders/retriever_spider.py
+++ src/retriever/retriever/spiders/retriever_spider.py
@@ -1,11 +1,12 @@
+from os import path
 import scrapy
-from scrapy.crawler import CrawlerProcess
 
 
 class RetrieverSpider(scrapy.Spider):
     name = "TvPrice"
 
     def start_requests(self):
+        
         urls = [
             'https://www.johnlewis.com/lg-oled55cs6la-2022-oled-hdr-4k-ultra-hd-smart-tv-55-inch-with-freeview-hd-freesat-hd-dolby-atmos-black/p109457439?s_ppc=1dx43700074457397082_brand_technology_BAU&tmad=c&tmcampid=2&gclid=Cj0KCQjwn9CgBhDjARIsAD15h0DRAbPPScq8_oelL148dXwzp3FJ2lT1egAwsI5cCZSKJCuOJPXA0q0aAg55EALw_wcB&gclsrc=aw.ds',
         ]
@@ -13,21 +14,13 @@ class RetrieverSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, headers=headers, callback=self.parse)
             
-            
     def parse(self, response):
-        for price in response.css("div.Price_base__1OoOa"):
+        for price in response.css("div.Price_base__7xOse"):
             print('Scrapy begins crawling')
             yield{
                 'prod_price': price.css("span::text").get(),
             }
-    
-process = CrawlerProcess(settings={
-    "FEEDS": {
-        "items.json": {"format": "json"},
-    },
-})
-process.crawl(RetrieverSpider)
-process.start()
+
     # def parse(self, response):
     #     filename = f'ProductPage.html'
     #     Path(filename).write_bytes(response.body)
